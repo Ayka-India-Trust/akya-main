0674b1122016582835238d03f7b4979b619296d2
amaan8429 <mohdamaan8429@gmail.com>
2024-05-17 19:18:15 +0530

    registration form idea dropped

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,4 +1,4 @@
-import RegisterForm from "@/components/register-form.tsx/form";
+import RegisterForm from "@/components/auth/register-form";
 import React from "react";
 
 export default function RegisterPage() {
--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -8,14 +8,3 @@ export const LoginSchema = z.object({
     message: "Password is required",
   }),
 });
-
-export const RegisterSchema = z.object({
-  firstName: z.string().min(1, "First name is required"),
-  lastName: z.string().min(1, "Last name is required"),
-  email: z.string().min(1, "Email is required").email("Invalid email address"),
-  country: z.string().min(1, "Country is required"),
-  street: z.string().min(1, "Street is required"),
-  city: z.string().min(1, "City is required"),
-  state: z.string().min(1, "State is required"),
-  zip: z.string().min(1, "Zip is required"),
-});
